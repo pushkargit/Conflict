abee96867be58d1fd1f52a9c99db465d8ac0d559
pushkarsa <pushkar04cse@gmail.com>
2019-11-13 02:15:02 +0530

    adding 1

--- src/com/conflict/Hello.java
+++ src/com/conflict/Hello.java
@@ -1,10 +1,12 @@
-package com.conflict;
+ 
 
 public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("adding master");
+		System.out.println(1);
+		
 
 	}
 
